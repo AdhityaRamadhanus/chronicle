341f3ce4741be9952a8c6b9d951d7b4d896fcec4
adhityaramadhanus <adhitya.ramadhanus@gmail.com>
2018-11-02 22:39:14 +0700

    add story service

---
+++ story/service.go
@@ -0,0 +1,128 @@
+package story
+
+import (
+	"database/sql"
+	"log"
+	"strconv"
+
+	"github.com/adhityaramadhanus/chronicle"
+	"github.com/adhityaramadhanus/chronicle/function"
+	"github.com/pkg/errors"
+)
+
+var (
+	//ErrNoStoryFound sub-domain specific error
+	ErrNoStoryFound = errors.New("Cannot find Story")
+)
+
+//Service provide an interface to story domain service
+type Service interface {
+	CreateStory(story chronicle.Story) (createdStory chronicle.Story, err error)
+	UpdateStory(story chronicle.Story) (updatedStory chronicle.Story, err error)
+	GetStories(filter chronicle.StoryFilterOptions, option chronicle.PagingOptions) (chronicle.Stories, int, error)
+	GetStoryByID(id int) (chronicle.Story, error)
+	GetStoryBySlug(slug string) (chronicle.Story, error)
+	DeleteStoryByID(id int) error
+}
+
+func NewService(storyRepository chronicle.StoryRepository) Service {
+	return &service{
+		storyRepository: storyRepository,
+	}
+}
+
+type service struct {
+	storyRepository chronicle.StoryRepository
+}
+
+func (s *service) CreateStory(story chronicle.Story) (createdStory chronicle.Story, err error) {
+	defer func() {
+		if err != nil && err != ErrNoStoryFound {
+			err = errors.Wrap(err, function.GetFunctionName(s.CreateStory))
+		}
+	}()
+
+	return s.storyRepository.Insert(story)
+}
+
+func (s *service) UpdateStory(story chronicle.Story) (updatedStory chronicle.Story, err error) {
+	defer func() {
+		if err != nil && err != ErrNoStoryFound {
+			err = errors.Wrap(err, function.GetFunctionName(s.UpdateStory))
+		}
+	}()
+
+	return s.storyRepository.Update(story)
+}
+
+func (s *service) GetStories(filter chronicle.StoryFilterOptions, option chronicle.PagingOptions) (stories chronicle.Stories, storiesCount int, err error) {
+	defer func() {
+		if err != nil && err != ErrNoStoryFound {
+			err = errors.Wrap(err, function.GetFunctionName(s.GetStories))
+		}
+	}()
+
+	// filter by status only
+	if filter.Status != "" && filter.Topic == "" {
+		log.Println("HERE", filter.Status)
+		return s.storyRepository.FindByStatus(filter.Status, option)
+	}
+
+	// filter by topic only
+	if filter.Topic != "" {
+		topicId, _ := strconv.Atoi(filter.Topic)
+		return s.storyRepository.FindByTopicAndStatus(topicId, filter.Status, option)
+	}
+
+	return s.storyRepository.All(option)
+}
+
+func (s *service) GetStoryByID(id int) (story chronicle.Story, err error) {
+	defer func() {
+		if err != nil && err != ErrNoStoryFound {
+			err = errors.Wrap(err, function.GetFunctionName(s.GetStoryByID))
+		}
+	}()
+
+	story, err = s.storyRepository.Find(id)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return story, ErrNoStoryFound
+		default:
+			return story, err
+		}
+	}
+
+	return story, nil
+}
+
+func (s *service) GetStoryBySlug(slug string) (story chronicle.Story, err error) {
+	defer func() {
+		if err != nil && err != ErrNoStoryFound {
+			err = errors.Wrap(err, function.GetFunctionName(s.GetStoryBySlug))
+		}
+	}()
+
+	story, err = s.storyRepository.FindBySlug(slug)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return story, ErrNoStoryFound
+		default:
+			return story, err
+		}
+	}
+
+	return story, nil
+}
+
+func (s *service) DeleteStoryByID(id int) (err error) {
+	defer func() {
+		if err != nil && err != ErrNoStoryFound {
+			err = errors.Wrap(err, function.GetFunctionName(s.DeleteStoryByID))
+		}
+	}()
+
+	return s.storyRepository.Delete(id)
+}
---
+++ story/service_test.go
@@ -0,0 +1,284 @@
+package story_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+
+	"github.com/adhityaramadhanus/chronicle"
+	"github.com/adhityaramadhanus/chronicle/config"
+	"github.com/adhityaramadhanus/chronicle/storage/postgre"
+	"github.com/adhityaramadhanus/chronicle/story"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	storyService    story.Service
+	storyRepository *postgre.StoryRepository
+	topicRepository *postgre.TopicRepository
+	// specific test case var
+	storyId int
+	topics  chronicle.Topics
+)
+
+// make test kind of idempotent
+func setupDatabase(db *sqlx.DB) {
+	_, err := db.Query("DELETE FROM topic_stories")
+	if err != nil {
+		log.Fatal("Failed to setup database ", errors.Wrap(err, "Failed in delete from topic_stories"))
+	}
+
+	_, err = db.Query("DELETE FROM stories")
+	if err != nil {
+		log.Fatal("Failed to setup database ", errors.Wrap(err, "Failed in delete from stories"))
+	}
+	_, err = db.Query("DELETE FROM topics")
+	if err != nil {
+		log.Fatal("Failed to setup database ", errors.Wrap(err, "Failed in delete from topics"))
+	}
+
+	_, err = db.Query(
+		`INSERT INTO topics (name, slug, createdat, updatedat)
+		VALUES ('Pemilu 2019', 'pemilu-2019', now(), now()),
+		('Pilkada 2019', 'pilkada-2019', now(), now()),
+		('Jokowi 2019', 'jokowi-2019', now(), now()),
+		('Prabowo 2019', 'prabowo-2019', now(), now())`)
+
+	if err != nil {
+		log.Fatal("Failed to setup database ", errors.Wrap(err, "Failed in filling topics table"))
+	}
+}
+
+func TestMain(m *testing.M) {
+	// log.SetLevel(log.WarnLevel)
+	if err := config.Init("testing", []string{"../config/testing"}); err != nil {
+		log.Fatal(err)
+	}
+
+	pgConnString := fmt.Sprintf(
+		`host=%s 
+		port=%d 
+		user=%s 
+		password=%s 
+		dbname=%s 
+		sslmode=%s`,
+		viper.GetString("database.host"),
+		viper.GetInt("database.port"),
+		viper.GetString("database.user"),
+		viper.GetString("database.password"),
+		viper.GetString("database.dbname"),
+		viper.GetString("database.sslmode"),
+	)
+
+	db, err := sqlx.Open("postgres", pgConnString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	setupDatabase(db)
+
+	// Repositories
+	storyRepository = postgre.NewStoryRepository(db, "stories")
+	topicRepository = postgre.NewTopicRepository(db, "topics")
+
+	storyService = story.NewService(storyRepository)
+
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestCreateStoryIntegration(t *testing.T) {
+	topics, _, err := topicRepository.All(chronicle.PagingOptions{
+		Limit:  3,
+		Offset: 0,
+		SortBy: "createdAt",
+		Order:  "asc",
+	})
+
+	if err != nil {
+		t.Error("Failed to get topics to for stories", err)
+	}
+
+	stories := chronicle.Stories{
+		chronicle.Story{
+			Title: "Dikalahkan Jepang, Timnas U-19 Gagal ke Piala Dunia",
+			Slug:  "dikalahkan-jepang-timnas-u-19-gagal-ke-piala-dunia",
+			Content: `Asa Timnas U-19 Indonesia mentas di Piala Dunia U-20 2019 pupus. 
+						 Berlaga melawan Jepang pada perempat final Piala Asia U-19 2018 di Stadion Ut`,
+			Reporter: "Adhitya Ramadhanus",
+			Editor:   "Adhitya Ramadhanus",
+			Author:   "Adhitya Ramadhanus",
+			Media:    []byte("{}"),
+			Excerpt:  "Timnas Gagal melaju ke pialla dunia",
+			Status:   chronicle.StoryPublishStatus,
+			Topics:   topics,
+		},
+		chronicle.Story{
+			Title:    "Test aja",
+			Slug:     "test-aja",
+			Content:  "Bertiga melawan jepang pada perempat final",
+			Reporter: "Adhitya Ramadhanus",
+			Editor:   "Adhitya Ramadhanus",
+			Author:   "Adhitya Ramadhanus",
+			Media:    []byte("{}"),
+			Status:   chronicle.StoryDraftStatus,
+			Excerpt:  "Timnas Gagal melaju ke pialla dunia",
+			Topics:   topics[1:3],
+		},
+	}
+
+	for _, story := range stories {
+		createdStory, err := storyService.CreateStory(story)
+
+		// take one topic, save its id to test getTopicByID later
+		storyId = createdStory.ID
+
+		if err != nil {
+			t.Error("Failed to create story", err)
+		}
+
+		assert.Equal(t, createdStory.Content, story.Content)
+	}
+}
+
+func TestGetStoriesIntegration(t *testing.T) {
+	testCases := []struct {
+		ExpectedStoriesCount int
+		ExpectedStoriesSlugs []string
+		FilterOption         chronicle.StoryFilterOptions
+		PagingOption         chronicle.PagingOptions
+	}{
+		{
+			ExpectedStoriesCount: 2,
+			ExpectedStoriesSlugs: []string{
+				"test-aja",
+				"dikalahkan-jepang-timnas-u-19-gagal-ke-piala-dunia",
+			},
+			FilterOption: chronicle.StoryFilterOptions{},
+			PagingOption: chronicle.PagingOptions{
+				SortBy: "createdAt",
+				Order:  "desc",
+				Limit:  2,
+				Offset: 0,
+			},
+		},
+		{
+			ExpectedStoriesCount: 1,
+			ExpectedStoriesSlugs: []string{
+				"test-aja",
+			},
+			FilterOption: chronicle.StoryFilterOptions{
+				Status: chronicle.StoryDraftStatus,
+			},
+			PagingOption: chronicle.PagingOptions{
+				SortBy: "createdAt",
+				Order:  "asc",
+				Limit:  1,
+				Offset: 0,
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		stories, _, err := storyService.GetStories(testCase.FilterOption, testCase.PagingOption)
+		if err != nil {
+			t.Error("Failed to create topic", err)
+		}
+		assert.Equal(t, len(stories), testCase.ExpectedStoriesCount)
+
+		for idx, story := range stories {
+			assert.Equal(t, story.Slug, testCase.ExpectedStoriesSlugs[idx])
+		}
+	}
+}
+
+func TestGetStoryByIDIntegration(t *testing.T) {
+	testCases := []struct {
+		StoryId           int
+		ExpectedError     bool
+		ExpectedErrorType error
+	}{
+		{
+			StoryId:       storyId,
+			ExpectedError: false,
+		},
+		{
+			StoryId:           storyId + 1,
+			ExpectedError:     true,
+			ExpectedErrorType: story.ErrNoStoryFound,
+		},
+	}
+
+	for _, test := range testCases {
+		_, err := storyService.GetStoryByID(test.StoryId)
+
+		if test.ExpectedError {
+			assert.EqualError(t, err, test.ExpectedErrorType.Error(), "Should return ErrStoryNotFound")
+		} else {
+			if err != nil {
+				t.Error("Failed to get story by id", err)
+			}
+		}
+	}
+}
+
+func TestGetTopicBySlugIntegration(t *testing.T) {
+	testCases := []struct {
+		Slug              string
+		ExpectedError     bool
+		ExpectedErrorType error
+	}{
+		{
+			Slug:          "dikalahkan-jepang-timnas-u-19-gagal-ke-piala-dunia",
+			ExpectedError: false,
+		},
+		{
+			Slug:              "testaja",
+			ExpectedError:     true,
+			ExpectedErrorType: story.ErrNoStoryFound,
+		},
+	}
+
+	for _, test := range testCases {
+		_, err := storyService.GetStoryBySlug(test.Slug)
+
+		if test.ExpectedError {
+			assert.EqualError(t, err, test.ExpectedErrorType.Error(), "Should return ErrStoryNotFound")
+		} else {
+			if err != nil {
+				t.Error("Failed to get story by slug", err)
+			}
+		}
+	}
+}
+
+func TestUpdateStoryIntegration(t *testing.T) {
+	story, err := storyRepository.Find(storyId)
+	if err != nil {
+		t.Error("Failed to get story to update", err)
+	}
+
+	story.Status = chronicle.StoryPublishStatus
+	story.Editor = "Bukan Adhitya Ramadhanus"
+
+	updatedStory, err := storyService.UpdateStory(story)
+
+	if err != nil {
+		t.Error("Failed to update story", err)
+	}
+
+	assert.Equal(t, updatedStory.Status, story.Status)
+	assert.Equal(t, updatedStory.Editor, story.Editor)
+}
+
+func TestDeleteStoryIntegration(t *testing.T) {
+	if err := storyService.DeleteStoryByID(storyId); err != nil {
+		t.Error("Failed to delete story", err)
+	}
+}
