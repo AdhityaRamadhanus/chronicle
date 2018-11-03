7fe47fbd02d98d8fc1465eeeb3a2065899fa1f9d
adhityaramadhanus <adhitya.ramadhanus@gmail.com>
2018-11-03 17:00:03 +0700

    move script

---
+++ script/run_migration/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"sort"
+
+	"github.com/adhityaramadhanus/chronicle/config"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	godotenv.Load()
+	config.Init(os.Getenv("ENV"), []string{})
+}
+
+func main() {
+	pgConnString := fmt.Sprintf(`
+		host=%s 
+		port=%d 
+		user=%s 
+		password=%s 
+		dbname=%s 
+		sslmode=%s`,
+		viper.GetString("database.host"),
+		viper.GetInt("database.port"),
+		viper.GetString("database.user"),
+		viper.GetString("database.password"),
+		viper.GetString("database.dbname"),
+		viper.GetString("database.sslmode"),
+	)
+
+	db, err := sqlx.Open("postgres", pgConnString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.WithFields(log.Fields{
+		"database": "postgres",
+		"host":     viper.GetString("database.host"),
+		"port":     viper.GetString("database.port"),
+	}).Info("Connected to postgres")
+
+	migrationDir := "storage/postgre/migration"
+	migrationFiles := []string{}
+	files, _ := ioutil.ReadDir(migrationDir)
+	for _, file := range files {
+		if !file.IsDir() {
+			migrationFiles = append(migrationFiles, file.Name())
+		}
+	}
+
+	sort.Sort(sort.StringSlice(migrationFiles))
+	for _, migrationFile := range migrationFiles {
+		log.Info("Running ", migrationFile)
+		filePath := path.Join(migrationDir, migrationFile)
+		file, err := os.Open(filePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fileBytes, _ := ioutil.ReadAll(file)
+		file.Close()
+
+		sqlQuery := string(fileBytes)
+		_, err = db.Queryx(sqlQuery)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
